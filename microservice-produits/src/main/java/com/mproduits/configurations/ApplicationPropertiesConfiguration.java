dc0024a4b7c8aa8752e76dd7a8f0f850618d7780
SlimGhraibi <slim.ghraibi1991@gmail.com>
2023-02-18 21:14:45 +0100

    .

---
+++ microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
@@ -0,0 +1,12 @@
+package com.mproduits.configurations;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("mes-configs")
+@Data
+public class ApplicationPropertiesConfiguration {
+    private int limitDeProduits;
+}
